d7fae9274b67cf32c9add0bf30181e9c5edce5f3
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-03-29 15:05:20 +0200

    clearScreen

--- src/chat/server/Server.java
+++ src/chat/server/Server.java
@@ -176,10 +176,16 @@ public class Server {
 		}
 		
 	}
+
+	public static void clearScreen() {  
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();  
+	}
 	
 	
 	public static void main(String[] args) {
 		try {
+			clearScreen();
 			new Server();
 		} catch (IOException e) {
 			System.out.println("Erreur de demarrage du serveur");
