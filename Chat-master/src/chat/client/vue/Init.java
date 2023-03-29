d7091a3a0e73e4c1e5fb1af3664adcdbd13ff6d6
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-03-29 09:25:57 +0200

    tentative chat avec salon...

---
+++ Chat-master/src/chat/client/vue/Init.java
@@ -0,0 +1,19 @@
+package chat.client.vue;
+
+import java.io.File;
+import java.io.IOException;
+
+import chat.client.Model.ModelConnexion;
+
+public class Init {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) {
+		System.out.println(new File("").getAbsolutePath()+"/images/16x16");
+
+	}
+
+}
---
+++ Chat-master/src/chat/server/BD/testBD.java
@@ -0,0 +1,16 @@
+package chat.server.BD;
+
+public class testBD {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(Utils.listeContact("bng"));
+		System.out.println(Utils.listeContact("sine"));
+		
+		
+	}
+
+}
